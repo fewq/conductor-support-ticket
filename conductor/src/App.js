28ddac2092c7d52c918bdcbd22174c29e4d16749
Thaddeus <thaddeus.phua@gmail.com>
2019-02-22 00:15:49 +0800

    added login form and styling

--- conductor/src/App.js
+++ conductor/src/App.js
@@ -8,20 +8,37 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Sign in to Conductor ticket support</h2>
+          {/* <input type="email" placeholder="Email" id="email_field" />
+          <input type="password" placeholder="Password" id="password_field" /> */}
+          <form className="Form">
+            <input
+              type="email"
+              placeholder="Email"
+              id="email_field"
+              autoComplete="email"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password_field"
+              autoComplete="current-password"
+              required
+            />
+            <button onClick={this.login}>Login</button>
+          </form>
         </header>
       </div>
     );
+  }
+
+  login() {
+    var raw_email = document.getElementById("email_field").value;
+    var raw_password = document.getElementById("password_field").value;
+    console.log("Login button pressed");
+    console.log("Email: " + raw_email);
+    console.log("Password: " + raw_password);
   }
 }
 
