445f2ab07d44a2ba29d7478e775718ad5a9271a9
thaddeusphua <thaddeus.phua@gmail.com>
2019-02-23 01:22:16 +0800

    added different pages

---
+++ conductor/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
---
+++ conductor/src/components/Secret.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Secret extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>WEW secret area</h1>
+      </div>
+    );
+  }
+}
+
+export default Secret;
