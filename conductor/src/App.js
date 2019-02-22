6cd560f097ee6ef1c16becf10b65665d62de17b5
thaddeusphua <thaddeus.phua@gmail.com>
2019-02-23 01:22:43 +0800

    added mainComponent function and removed old login solution

--- conductor/src/App.js
+++ conductor/src/App.js
@@ -1,44 +1,33 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/Main";
+import Secret from "./components/Secret";
+import NotFound from "./components/NotFound";
 
 class App extends Component {
   render() {
+    let mainComponent = "";
+    switch (this.props.location) {
+      case "":
+        mainComponent = <Main />;
+        break;
+      case "secret":
+        mainComponent = <Secret />;
+        break;
+      default:
+        mainComponent = <NotFound />;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Sign in to Conductor ticket support</h2>
-          {/* <input type="email" placeholder="Email" id="email_field" />
-          <input type="password" placeholder="Password" id="password_field" /> */}
-          <form className="Form">
-            <input
-              type="email"
-              placeholder="Email"
-              id="email_field"
-              autoComplete="email"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              id="password_field"
-              autoComplete="current-password"
-              required
-            />
-            <button onClick={this.login}>Login</button>
-          </form>
+          Welcome to react, {this.props.name}
+          {mainComponent}
         </header>
       </div>
     );
-  }
-
-  login() {
-    var raw_email = document.getElementById("email_field").value;
-    var raw_password = document.getElementById("password_field").value;
-    console.log("Login button pressed");
-    console.log("Email: " + raw_email);
-    console.log("Password: " + raw_password);
   }
 }
 
