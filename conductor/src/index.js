05c5943cbeb9fab66f935f4f795ff8b280f476e5
thaddeusphua <thaddeus.phua@gmail.com>
2019-02-23 01:23:10 +0800

    added state magic to index.js

--- conductor/src/index.js
+++ conductor/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let state = {};
+Window.setState = changes => {
+  state = Object.assign({}, state, changes); //updates the state via changes
+
+  ReactDOM.render(<App {...state} />, document.getElementById("root"));
+};
+
+/* eslint no-restricted-globals: 0*/
+let initialState = {
+  name: "Ted",
+  location: location.pathname.replace(/^\/?|\/$/g, "")
+  //if it still doesn't work change location to window.location
+};
+
+Window.setState(initialState);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
