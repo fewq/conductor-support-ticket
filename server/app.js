dd3df411b72d957044a7abdc402a202c288beb2a
dyuanjia <32560891+dyuanjia@users.noreply.github.com>
2019-04-03 03:42:57 +0800

    Kanban backend (#20)
    * identified error
    
    * moved store to index
    
    * database reflection on kanban on startup

--- server/app.js
+++ server/app.js
@@ -7,50 +7,52 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyParser.json());
 
 ///////////////////////////// EMAIL SENDER ///////////////////////////
 app.post("/api/notify", (req, res) => {
-    // for now response is empty
-    const output = `
+  // for now response is empty
+  const output = `
     <h3>Ticket Title: ${req.body.title}</h3>
     <p>Your ticket status: ${req.body.status}</p>
   `;
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: "rashad.green@ethereal.email",
-            pass: "3tgthkxNKmvs5hWjSB"
-        },
-        tls: {
-            rejectUnauthorized: false
-        } // necessary for sending from local host
-    });
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "rashad.green@ethereal.email",
+      pass: "3tgthkxNKmvs5hWjSB"
+    },
+    tls: {
+      rejectUnauthorized: false
+    } // necessary for sending from local host
+  });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Admin Test" <rashad.green@ethereal.email>', // sender address
-        to: "hfut07+7xzrkpohotcqs@sharklasers.com", // list of receivers
-        subject: "Test Notification", // Subject line
-        text: "Testing", // plain text body
-        html: output // html body
-    };
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Admin Test" <rashad.green@ethereal.email>', // sender address
+    to: "hfut07+7xzrkpohotcqs@sharklasers.com", // list of receivers
+    subject: "Test Notification", // Subject line
+    text: "Testing", // plain text body
+    html: output // html body
+  };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    });
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  });
 });
 
 ///////////////////////////// DATABASE ///////////////////////////
@@ -60,16 +62,18 @@ app.use(cors());
 const mongoose = require("mongoose");
 const config = require("./config/atlas.js");
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, {
+mongoose
+  .connect(config.DB, {
     useNewUrlParser: true
-}).then(
+  })
+  .then(
     () => {
-        console.log("Database is connected");
+      console.log("Database is connected");
     },
     err => {
-        console.log("Can not connect to the database" + err);
+      console.log("Can not connect to the database" + err);
     }
-);
+  );
 
 // set up routes & port
 const ticketRoutes = require("./routers/ticket.route");
--- src/components/admin/client/EditTicket.js
+++ src/components/admin/client/EditTicket.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Edit extends Component {
   constructor(props) {
@@ -10,30 +10,32 @@ export default class Edit extends Component {
       formType: "",
       topics: [],
       description: ""
-    }
+    };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/ticket/view/'+this.props.match.params.id)
-        .then(response => {
-          console.log("retrieved json response for editing" );
-          console.log(response);
-            this.setState({ 
-              formType: response.data.formType, 
-              topics: response.data.topics,
-              description: response.data.description });
-              console.log("retrieved state for editing" );
-              console.log(this.state);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    }
+    axios
+      .get("http://localhost:4000/ticket/view/" + this.props.match.params.id)
+      .then(response => {
+        console.log("retrieved json response for editing");
+        console.log(response);
+        this.setState({
+          formType: response.data.formType,
+          topics: response.data.topics,
+          description: response.data.description
+        });
+        console.log("retrieved state for editing");
+        console.log(this.state);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
   onChangeDescription(e) {
     this.setState({
       description: e.target.value
-    })
+    });
   }
 
   onSubmit(e) {
@@ -43,39 +45,43 @@ export default class Edit extends Component {
       topics: this.state.topics,
       description: this.state.description
     };
-    axios.patch('http://localhost:4000/ticket/update/'+this.props.match.params.id)
-        .then(res => console.log(res.data))
-        .catch(function (error) {
-          console.log(error);
-      })
-    
-    this.props.history.push('/restricted');
+    axios
+      .patch(
+        "http://localhost:4000/ticket/update/" + this.props.match.params.id
+      )
+      .then(res => console.log(res.data))
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    this.props.history.push("/restricted");
   }
- 
+
   render() {
     return (
-        <div style={{ marginTop: 10 }}>
-            <h3 align="center">Edit Ticket</h3>
-            <form onSubmit={this.onSubmit}>
-            
-                <div className="form-group">
-                    <label htmlFor="description" style={{ display: "block" }}>
-                        Your Message
-                    </label>
-                    <textarea
-                        className="form-control"
-                        id="description"
-                        value={this.state.description}
-                        onChange={this.onChangeDescription}
-                    />
-                </div>
-                <div className="form-group">
-                    <input type="submit" 
-                      value="Update Ticket" 
-                      className="btn btn-primary"/>
-                </div>
-            </form>
-        </div>
-    )
+      <div style={{ marginTop: 10 }}>
+        <h3 align="center">Edit Ticket</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label htmlFor="description" style={{ display: "block" }}>
+              Your Message
+            </label>
+            <textarea
+              className="form-control"
+              id="description"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Update Ticket"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
