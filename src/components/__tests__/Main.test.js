cf0f4152fc26f35ace0d5fef8f5f9f80fb7a307a
lyqht <estee.tsw@gmail.com>
2019-04-11 00:33:05 +0800

    added test file for ticketlisttablerow

--- src/components/__tests__/Main.test.js
+++ src/components/__tests__/Main.test.js
@@ -1,31 +1,11 @@
 import React from 'react'
 import Main from "../Main"
-import { render, fireEvent, cleanup, queryByTestId } from 'react-testing-library'
-import { createMemoryHistory } from 'history'
-import { Link, Route, Router} from 'react-router-dom'
+import { render, fireEvent, cleanup } from 'react-testing-library'
 import {toBeInTheDocument, toHaveClass} from 'jest-dom'
 
 expect.extend({toBeInTheDocument, toHaveClass})
 
 afterEach(cleanup)
-
-// this is a handy function that I would utilize for any component
-// that relies on the router being in context
-function renderWithRouter(
-  ui,
-  {
-    route = '/',
-    history = createMemoryHistory({ initialEntries: [route] }),
-  } = {}
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    history,
-  }
-}
 
 var mockAuth = {
     isAuthenticated: () => true
---
+++ src/components/__tests__/TicketListTableRow.test.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import { render, fireEvent, cleanup, waitForElement } from 'react-testing-library'
+import TableRow from '../client/TicketListTableRow';
+import renderer from 'react-test-renderer';
+import {toBeInTheDocument, toHaveClass} from 'jest-dom'
+
+expect.extend({toBeInTheDocument, toHaveClass})
+
+afterEach(cleanup);
+
+var obj = {
+    dateOfCreation: new Date(),
+    formType: "bug",
+    title: "some title",
+    statusToClient: "Pending Admin"
+}
+
+
+
+describe("retrieves and renders props correctly", () => {
+    test("Date", () => {
+        const { getByText } = render(<BrowserRouter>
+            <TableRow obj={obj} />
+            </BrowserRouter>)
+        const date = getByText(/2019/);
+        expect(date).toBeDefined();
+    })
+
+    test("Title", () => {
+        const { getByText } = render(<BrowserRouter>
+            <TableRow obj={obj} />
+            </BrowserRouter>)
+        const title = getByText("some title");
+        expect(title).toBeDefined();
+    })
+
+    test("Form Type", () => {
+        const { getByText } = render(<BrowserRouter>
+            <TableRow obj={obj} />
+            </BrowserRouter>)
+        const formType = getByText("bug");
+        expect(formType).toBeDefined();
+    })
+
+    test("Ticket Status", () => {
+        const { getByText } = render(<BrowserRouter>
+            <TableRow obj={obj} />
+            </BrowserRouter>)
+        const statusToClient = getByText(/pending/i);
+        expect(statusToClient).toBeDefined();
+    })
+})
+
+test("has button to delete the ticket", () => {
+    const { getByText } = render(<BrowserRouter>
+        <TableRow obj={obj} />
+        </BrowserRouter>)
+    const deleteButton = getByText(/delete/i);
+    expect(deleteButton).toBeDefined();
+})
+
+test('snapshot testing', () => {
+    const tree = renderer
+      .create(<BrowserRouter>
+        <TableRow obj={obj} />
+        </BrowserRouter>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
