61641479da50b59111c1ccd194b1628605898b49
Sean Lew <seanlewtengsiong@gmail.com>
2019-04-11 23:43:25 +0800

    Modified server test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,9 +1,11 @@
-const expect = require ('expect');
-const request = require ('supertest');
+const expect = require('expect');
+const request = require('supertest');
 
-const{ObjectID} = require('mongodb');
-const app = require ('../app');
-let Ticket = require ('../models/ticket');
+const {
+   ObjectID
+} = require('mongodb');
+const app = require('../app');
+let Ticket = require('../models/ticket');
 
 
 const tickets = [{
@@ -15,43 +17,48 @@ const tickets = [{
    description: 'First test ticket',
    title: 'First test ticket',
    topics: [],
-   formType:'test1'
-},{
+   formType: 'test1'
+}, {
    _id: new ObjectID(),
-      statusToClient: 'test 2',
-      statusToAdmin: 'test 2',
-      createdBy: 'testing2@hotmail.com',
-      completedBy: 'test2',
-      description: 'Second test ticket',
-      title: 'second test ticket',
-      topics: [],
-      formType: 'test2'
+   statusToClient: 'test 2',
+   statusToAdmin: 'test 2',
+   createdBy: 'testing2@hotmail.com',
+   completedBy: 'test2',
+   description: 'Second test ticket',
+   title: 'second test ticket',
+   topics: [],
+   formType: 'test2'
 
 }]
 //Database is reset to the default 2 tickets declared on top BEFORE EACH test.
-before((done) => {
-   Ticket.deleteMany({}).then(() => {
-      return Ticket.insertMany(tickets);
-   }).then(() => {
-      done()
-   });
-});
+
 
 describe('POST /ticket', () => {
+   before((done) => {
+      Ticket.deleteMany({}).then(() => {
+         return Ticket.insertMany(tickets);
+      }).then(() => {
+         done()
+      });
+   });
    it('should create a new ticket without fileUpload', (done) => {
       var createdBy = 'testing 123';
       request(app)
          .post('/ticket/add')
-         .send({createdBy})
+         .send({
+            createdBy
+         })
          .expect(200)
          .expect((res) => {
             expect(res.body.createdBy).toBe(createdBy);
          })
-         .end((err, res) =>{
+         .end((err, res) => {
             if (err) {
                return done(err);
             }
-            Ticket.find({createdBy}).then((tickets) => {
+            Ticket.find({
+               createdBy
+            }).then((tickets) => {
                expect(tickets.length).toBe(1)
                expect(tickets[0].createdBy).toBe(createdBy);
                done();
@@ -152,43 +159,45 @@ describe('GET /ticket/email/:email', () => {
 describe('GET /ticket/view/:id', () => {
    it('should return ticket doc', (done) => {
       request(app)
-      .get(`/ticket/view/${tickets[0]._id.toHexString()}`)
-      .expect(200)
-      .expect((res) => {
-         expect(res.body.title).toBe(tickets[0].title);
-      })
-      .end(done);
+         .get(`/ticket/view/${tickets[0]._id.toHexString()}`)
+         .expect(200)
+         .expect((res) => {
+            expect(res.body.title).toBe(tickets[0].title);
+         })
+         .end(done);
    });
 
    it('should return 404 if ticket not found', (done) => {
       var hexId = new ObjectID().toHexString();
       request(app)
-      .get(`/ticket/view/${hexId}`)
-      .expect(404)
-      .end(done);
+         .get(`/ticket/view/${hexId}`)
+         .expect(404)
+         .end(done);
    });
 
    it('should return 500 for non-object ids', (done) => {
       request(app)
-      .get('/ticket/view/123abc')
-      .expect(500)
-      .end(done)
+         .get('/ticket/view/123abc')
+         .expect(500)
+         .end(done)
    });
 });
 
 describe('GET /ticket/view/:id/fileupload', (done) => {
    var createdBy = 'testing fileupload';
-   var hexId     
+   var hexId
    it('should return 200 retrieve data for fileuploads', (done) => {
-      Ticket.find({createdBy}).then((tickets) => {
+      Ticket.find({
+         createdBy
+      }).then((tickets) => {
          hexId = tickets[0]._id
          request(app)
             .get(`/ticket/view/${hexId}/fileupload`)
             .expect(200)
             .end(done);
       }).catch((e) => done(e));
-      
-      
+
+
    })
 });
 
@@ -201,31 +210,31 @@ describe('DELETE /ticket/delete/:id', () => {
          .expect((res) => {
             expect(res.body._id).toBe(hexId);
          })
-      .end((err, res) => {
-         if (err) {
-            return done(err) //error gets rendered by Mocha
-         }
+         .end((err, res) => {
+            if (err) {
+               return done(err) //error gets rendered by Mocha
+            }
 
-      Ticket.findById(hexId).then((ticket) => {
-         expect(ticket).toBeFalsy();
-         done();
-      }).catch((e) =>done(e));
+            Ticket.findById(hexId).then((ticket) => {
+               expect(ticket).toBeFalsy();
+               done();
+            }).catch((e) => done(e));
+         });
    });
-});
 
    it('should return 404 if ticket not found', (done) => {
       var hexId = new ObjectID().toHexString();
       request(app)
-      .delete(`/ticket/view/${hexId}`)
-      .expect(404)
-      .end(done);
+         .delete(`/ticket/view/${hexId}`)
+         .expect(404)
+         .end(done);
    });
 
    it('should return 500 if object id is invalid', (done) => {
       request(app)
-      .delete('/ticket/delete/123abc')
-      .expect(500)
-      .end(done)
+         .delete('/ticket/delete/123abc')
+         .expect(500)
+         .end(done)
    });
 });
 
@@ -233,7 +242,8 @@ describe('PATCH /ticket/update/:id', () => {
    it('should update the ticket', (done) => {
       var hexId = tickets[0]._id.toHexString();
       var newTicket = {
-         description: 'This should be the new description'}
+         description: 'This should be the new description'
+      }
       request(app)
          .patch(`/ticket/update/${hexId}`)
          .send(newTicket)
@@ -242,14 +252,14 @@ describe('PATCH /ticket/update/:id', () => {
             //custom expect calls
             expect(res.body.description).toBe(newTicket.description);
          })
-      .end(done);
+         .end(done);
    });
 
    it('should not upate ticket if cannot find', (done) => {
       var hexId = new ObjectID().toHexString();
       request(app)
-      .delete(`/ticket/update/${hexId}`)
-      .expect(404)
-      .end(done)
+         .delete(`/ticket/update/${hexId}`)
+         .expect(404)
+         .end(done)
    })
 });
